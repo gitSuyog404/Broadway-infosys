34eb1d5f9014ee968ff0e0f11900e18146e1c5b9
CodeSuyog <codesuyog0@gmail.com>
2024-05-30 08:59:46 +0545

    class 10 end - es6 basics to be continued

---
+++ Javascript/es6.js
@@ -0,0 +1,48 @@
+// let and const
+// arrow function
+// object literal (spread operators)
+// array
+
+// let numbers = [1,2,3,4,5];
+
+// foEach ma callback use hunxa so callback ma always arrow function banai halne.
+
+// variable_name.forEach(callbackfunction)
+
+// numbers.forEach((num)=>{
+//     console.log(num);
+// });
+
+// let sq = numbers.map((n)=> `<h1>${n}</h1>`)  jsx syntax ko example ;
+// console.log(sq);
+
+// let even = numbers.filter((n) => n%2==0);
+// console.log(even);
+
+// for (let num of numbers){
+
+// }
+
+// Reduce
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// numbers.reduce;
+// let result = numbers.reduce((sum, n) => sum + n, 8);
+
+// console.log(result);
+
+// Reduce chai mostly use hudaina mostly use hune vaneko chai filter ra map
+
+// .some ma chai kunai euta element le condition meet garyo vane pani true dinxa
+
+// let numbers = [1,2,3,4,5]
+
+// let result = numbers.some((n) => n%2==0);
+// console.log(result);
+
+// .every ma chai sabai element le condition meet garnu parxa
+
+let numbers = [1, 2, 3, 4, 5];
+let result = numbers.every((n) => n % 2 == 0);
+console.log(result);
