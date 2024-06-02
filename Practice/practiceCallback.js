6b64769e757d6421923f174387170fc3e24c8324
CodeSuyog <codesuyog0@gmail.com>
2024-06-02 08:08:55 +0545

    uncommitted changes error fix

--- Practice/practiceCallback.js
+++ Practice/practiceCallback.js
@@ -21,3 +21,25 @@
 //     console.log(`The area of the given values is ${result}cm2`);
 //   }
 // });
+
+// Write a program to find the area of the given length or breadth using callback
+
+let area = (length, breadth, callback) => {
+  setTimeout(() => {
+    if (length < 0 || b < 0) {
+      let message = "The given length or breadth should not be negative";
+      callback(message, null);
+    } else {
+      let area = length * breadth;
+      callback(null, area);
+    }
+  }, 2000);
+};
+
+area(10, 20, (err, result) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(`The area of the given values is ${result}cm2`);
+  }
+});
