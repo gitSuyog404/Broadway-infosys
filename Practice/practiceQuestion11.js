9d2254a7ef8bbed20c11796a6ae296183f40b6d2
CodeSuyog <codesuyog0@gmail.com>
2024-06-07 09:20:04 +0545

    added revision question11

---
+++ Practice/practiceQuestion11.js
@@ -0,0 +1,17 @@
+let users = [
+  { userid: 1, email: "johnwick@gmail.com" },
+  { userid: 2, email: "maxpayne@gmail.com" },
+];
+
+let blogs = [
+  { blogid: 1, text: "wick", postedBy: 1 },
+  { blogid: 2, text: "maxpayen", postedBy: 2 },
+];
+
+let getBlogByEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let userId = null;
+    });
+  });
+};
