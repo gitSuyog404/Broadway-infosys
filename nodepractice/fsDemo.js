51862b3e15fb18d776f241d7d8588fce2f3446ec
CodeSuyog <codesuyog0@gmail.com>
2024-06-12 08:40:20 +0545

    fs module practice end

--- nodepractice/fsDemo.js
+++ nodepractice/fsDemo.js
@@ -8,10 +8,32 @@ import fs from "fs";
 //   }
 // });
 
-fs.appendFile("./test.txt", "\n Third Line", (err) => {
+// fs.appendFile("./test.txt", "\n Third Line", (err) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log("File Written");
+//   }
+// });
+
+// fs.readFile("./test.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     // console.log(data.toString());
+//     console.log(data);
+//   }
+// });
+
+fs.unlink("./test.txt", (err) => {
   if (err) {
     console.log(err.message);
-  } else {
-    console.log("File Written");
   }
 });
+
+// fs.mkdir();  make new directory
+// rm.mkdir(); remove directory
+// fs.exists; checks whether the file exists or not
+
+let isExists = fs.existsSync("./test.txt");
+console.log(isExists);
