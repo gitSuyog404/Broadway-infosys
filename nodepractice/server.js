3471bd09ce95f38790c3579cc377dfcfec8b61db
CodeSuyog <codesuyog0@gmail.com>
2024-06-12 23:20:02 +0545

    class 20 assignment complete

--- nodepractice/server.js
+++ nodepractice/server.js
@@ -14,35 +14,74 @@
 //   console.log("server is running");
 // });
 
-import fs from "fs";
+// import fs from "fs";
+// import http from "http";
+
+// const server = http.createServer((req, res) => {
+//   // Check if the request is for the root URL or '/'
+//   if (req.url === "/" || req.url === "/index.html") {
+//     // Read the HTML file
+//     fs.readFile("index.html", (err, data) => {
+//       if (err) {
+//         // Handle error if file reading fails
+//         res.writeHead(404);
+//         res.end("File not found!");
+//       } else {
+//         // Set content type to HTML
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         // Write the contents of the HTML file to the response
+//         res.write(data);
+//         // End the response
+//         res.end();
+//       }
+//     });
+//   } else {
+//     // Handle other requests
+//     res.writeHead(404);
+//     res.end("Page not found!");
+//   }
+// });
+
+// // Start the server
+// server.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
+
 import http from "http";
+import fs from "fs";
+import path from "path";
 
 const server = http.createServer((req, res) => {
-  // Check if the request is for the root URL or '/'
-  if (req.url === "/" || req.url === "/index.html") {
-    // Read the HTML file
-    fs.readFile("index.html", (err, data) => {
-      if (err) {
-        // Handle error if file reading fails
-        res.writeHead(404);
-        res.end("File not found!");
-      } else {
-        // Set content type to HTML
-        res.writeHead(200, { "Content-Type": "text/html" });
-        // Write the contents of the HTML file to the response
-        res.write(data);
-        // End the response
-        res.end();
-      }
-    });
-  } else {
-    // Handle other requests
-    res.writeHead(404);
-    res.end("Page not found!");
+  let filePath = "";
+  let contentType = "text/html";
+
+  switch (req.url) {
+    case "/":
+      filePath = path.join(__dirname, "index.html");
+      break;
+
+    case "/about":
+      filePath = path.join(__dirname, "about.html");
+      break;
+
+    case "/contact":
+      filePath = path.join(__dirname, "contact.html");
+      break;
   }
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.write("Internal Server Error");
+      res.end();
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
-// Start the server
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
