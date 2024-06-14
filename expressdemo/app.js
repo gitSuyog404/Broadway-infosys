241a82d8641081c4f6ccb6f98bd64b448c0054a7
CodeSuyog <codesuyog0@gmail.com>
2024-06-14 09:08:46 +0545

    GET and POST practice in expressjs

--- expressdemo/app.js
+++ expressdemo/app.js
@@ -1,18 +1,23 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-const user = {
-  id: 1,
-  username: "JohnWick",
-};
+let users = [{ id: 1, username: "Johnwick" }];
 
 app.get("/", (req, res) => {
   res.send("<h3>Server is running</h3>");
 });
 
 app.get("/user", (req, res) => {
-  res.send(user);
+  res.send(users);
+});
+
+app.post("/adduser", (req, res) => {
+  // console.log(req.body);
+  // res.send("posted");
+  users.push(req.body);
+  res.send({ message: "USER ADDED" });
 });
 
 app.listen(3000, () => {
